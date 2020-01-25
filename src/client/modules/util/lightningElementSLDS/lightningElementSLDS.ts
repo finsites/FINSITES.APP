7e05892084aeb97f33dc2bbe8f3d7150d4fb04c3
Finsites <finsites@gmail.com>
2020-01-25 14:25:57 -0500

    Installed Synthetic Shadow Dom

--- src/client/modules/util/lightningElementSLDS/lightningElementSLDS.ts
+++ src/client/modules/util/lightningElementSLDS/lightningElementSLDS.ts
@@ -1,15 +1,15 @@
 import { LightningElement } from 'lwc';
 
 export default class LightningElementSLDS extends LightningElement {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		// Add SLDS CSS
-		let _sldsPath = '../resources/styles';
-		_sldsPath += '/salesforce-lightning-design-system.css';
-		const styles = document.createElement('link');
-		styles.href = _sldsPath;
-		styles.rel = 'stylesheet';
-		this.template.appendChild(styles);
-	}
+        // Add SLDS CSS
+        let _sldsPath = '../resources/styles';
+        _sldsPath += '/finsites-lightning-design-system.css';
+        const styles = document.createElement('link');
+        styles.href = _sldsPath;
+        styles.rel = 'stylesheet';
+        this.template.appendChild(styles);
+    }
 }
